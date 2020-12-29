3e1bcf23d4b22f673eaf7fa5fc6c70d65983fe23
Daniel Amaya <daniel_amaya7398@hotmail.com>
2020-12-29 18:22:32 -0500

    push supremo jeje varios cambios :v

---
+++ App/script/crud.js
@@ -0,0 +1,35 @@
+class Crud{
+    constructor(evento){        
+        this.arreglo[].push(evento);
+    }
+    let eventos = [Evento];
+    agregarEvento(){
+
+    }
+    editarEvento(){
+
+    }
+    eliminarEvento(){
+
+    }
+}
+evento = { nombre:"PIB cheeto", lugar:{ x:100, y:50}, descripcion: "Hola wangos", hora:4, Responsable:"Daniel" }
+console.log(evento);
+
+class Evento {
+    constructor(nombre, lugar{}, descricion, hora, responsable){
+        this.nombre = nombre;
+        this.lugar = lugar{};
+        this.descricion = descricion;
+        this.hora = hora;
+        this.responsable = responsable;
+    }
+
+}
+
+class Lugar{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
--- App/script/index.js
+++ App/script/index.js
@@ -1,7 +1,28 @@
+
+/*============NAVBAR============*/
+menu = document.getElementById("navMenu");
+//Click fuera de navBar button
+window.onclick = function(event) {
+	if (!(event.target.matches('.icon-bar-button')
+	|| event.target.matches('.icon-bar'))
+	&& menu.classList.contains("showMenu")) {
+		console.log(menu.className);
+		menu.classList.toggle("showMenu");
+		console.log(menu.className);
+	}
+}
+
+//Toggle navBar
+document.getElementById("btnMenu")
+  .addEventListener("click", function(){
+	menu.classList.toggle("showMenu");
+});
+
+/*============MAPA============*/
+
 var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 		osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		osm = L.tileLayer(osmUrl, {maxZoom: 20, attribution: osmAttrib});
 	var map = L.map('map').setView([21.04817, -89.64448], 18).addLayer(osm);
 	L.marker([21.04817, -89.64448])
-		.addTo(map)
-		marker.bindPopup('FMAT').openPopup();
+		.addTo(map).bindPopup('FMAT').openPopup();
